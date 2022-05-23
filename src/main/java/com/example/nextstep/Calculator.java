3a5bf7dc9a7a2f62a7f4e93d1f3c9375ff5c09c6
rheew <47098896+rheew@users.noreply.github.com>
2022-05-23 23:07:22 +0900

    refactor: 계산기 메서드 로직 분리

--- src/main/java/com/example/nextstep/Calculator.java
+++ src/main/java/com/example/nextstep/Calculator.java
@@ -1,31 +1,38 @@
 package com.example.nextstep;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Calculator {
 
     public int add(String param) {
-
         int result = 0;
+
+        if (isEmpty(param)) {
+            return result;
+        }
+
+        final List<Integer> nums = findInt(param);
+
+        result = nums.stream()
+                .reduce((x, y) -> x + y)
+                .orElse(null);
+
+        return result;
+    }
+
+    private List<Integer> findInt(String s) {
         String[] separator = {",", ":", "\n"};
         String regex = Arrays.stream(separator)
                 .reduce((x, y) -> x + "|" + y).orElse(null);
 
-        if (param == null || param.isEmpty()) {
-            return 0;
-        }
+        return Arrays.stream(s.split(regex))
+                .map(i -> Integer.parseInt(i))
+                .collect(Collectors.toList());
+    }
 
-        if(param.length() == 1) {
-            return Integer.parseInt(param);
-        }
-
-        if (param.contains(",")) {
-            result = Arrays.stream(param.split(regex))
-                    .mapToInt(item -> Integer.parseInt(item))
-                    .sum();
-        }
-
-        return result;
+    private boolean isEmpty(String s) {
+        return s == null || s.isEmpty();
     }
 }
